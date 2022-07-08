c7e7e2b564c0b9d8e89bcd5653940d04b8213a13
George <jacemilrp@gmail.com>
2022-07-08 18:32:16 +0100

    Rework

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-#   r = adapter.send(request, **kwargs)
-#  File "/usr/local/lib/python3.9/dist-packages/requests/adapters.py", line 516, in send
-#    raise ConnectionError(e, request=request)
-#requests.exceptions.ConnectionError: HTTPSConnectionPool(host='www.findapprenticeship.service.gov.uk', port=443): Max retries exceeded with url: /apprenticeships?SearchField=All&Keywords=Software%20Developer&Location=NG17%202BB&WithinDistance=30&ApprenticeshipLevel=Advanced&DisabilityConfidentOnly=false&Latitude=53.132415&Longitude=-1.263422&Hash=835434138&SearchMode=Keyword&Category=&LocationType=NonNational&GoogleMapApiKey=AIzaSyAg5lwS3ugdAVGf5gdgNvLe_0-7XcMICIM&sortType=RecentlyAdded&SearchAction=Sort&resultsPerPage=50&DisplayDescription=true&DisplayDistance=true&DisplayClosingDate=true&DisplayStartDate=true&DisplayApprenticeshipLevel=true&DisplayWage=true (Caused by NewConnectionError('<urllib3.connection.HTTPSConnection object at 0x7fabbc04a5e0>: Failed to establish a new connection: [Errno -3] Temporary failure in name resolution'))
-
 import json
+from tokenize import endpats
 from bs4 import BeautifulSoup
+from grpc import FutureCancelledError
 import requests
 import regex
 from time import sleep
@@ -42,14 +39,7 @@ class ApprenticeScraper:
             sleep(self.timeToWait * 60)
 
     def main(self):
-        print(f"{self.OKBLUE}[{self.now()}][INFO]{self.ENDC} Making web request")
-        try:
-            self.r = requests.get(self.url)
-        except ConnectionError as err:
-            print(f"{self.FAIL}[{self.now()}][ERROR]{self.ENDC} Connection error (Rate Limited), Canceling this loop.")
-            self.sendLogWebhook(err)
-            return
-        print(f"{self.OKBLUE}[{self.now()}][INFO]{self.ENDC} Web request completed")
+        self.r = self.makeRequest(self.url)
 
         if (self.r.status_code != 200):
             print(f"{self.FAIL}[{self.now()}][INFO]{self.ENDC} An error occured. Status Code: {self.r.status_code}")
@@ -75,6 +65,25 @@ class ApprenticeScraper:
             appWage = self.formatStr(res.find("li", attrs={"data-show":"DisplayWage"}).text)
             appURL = f"https://www.findapprenticeship.service.gov.uk/{res.h2.a['href']}"
 
+            subR = self.makeRequest(appURL, True)
+            ss = BeautifulSoup(subR.text, "lxml")
+
+            qualsReq = ss.find("div", id="vacancy-qualifications-required").contents
+            provider = ss.find("p", id="vacancy-provider-name").text
+            compURL = ss.find("a", id="vacancy-employer-website")["href"]
+            startDate = ss.find("p", id="vacancy-start-date").text
+            fullDesc = self.formatStr(ss.find("div", id="vacancy-full-descrpition").text)
+
+            fullDesc = self.boldStr(fullDesc, "What will the apprentice be doing?")
+            fullDesc = self.boldStr(fullDesc, "What training will the apprentice take and what qualification will the apprentice get at the end?")
+            fullDesc = self.boldStr(fullDesc, "What is the expected career progression after this apprenticeship?")
+
+
+            for i in qualsReq:
+                if str(i) == "<br/>": qualsReq.remove(i)
+
+            for i in range(len(qualsReq)): qualsReq[i] = self.formatStr(qualsReq[i])
+            
             apprenticeships.update({appCompany: {
                 "Title": appTitle,
                 "Company": appCompany,
@@ -87,7 +96,13 @@ class ApprenticeScraper:
                 "Start Date": appStartDate,
                 "Apprenticeship Level": appLevel,
                 "Wage": appWage,
-                "URL": appURL}})
+                "URL": appURL,
+                "Provider": provider,
+                "Company URL": compURL,
+                "Start Date": startDate,
+                "Qualifications Required": qualsReq,
+                "Full Description": fullDesc
+                }})
 
             # print(f"Title: {appTitle}\nCompany: {appCompany}\nDate Posted: {appDatePosted}\nNumber of  Positions: {appNumPositions}\nDistance: {appDistance} miles\nClosing Date: {appClosingDate}  \nPotential Start Date: {appStartDate}\nLevel: {appLevel}\nWage: {appWage}\nDescription   (Short): {appShortDesc}\n")
 
@@ -107,15 +122,21 @@ class ApprenticeScraper:
             embed.set_footer(text="National Apprenticeship Site Scraper", icon_url="https://cdn.icon-icons.com/icons2/2389/PNG/512/gov_uk_logo_icon_145225.png")
 
             embed.add_embed_field(name="Title", value=v["Title"], inline=self.embedInline)
-            embed.add_embed_field(name="Company", value=v["Company"], inline=self.embedInline)
+            embed.add_embed_field(name="Company", value=f"[{v['Company']}]({v['Company URL']})", inline=self.embedInline)
             embed.add_embed_field(name="Date Posted", value=v["Date Posted"], inline=self.embedInline)
             embed.add_embed_field(name="Number of Positions", value=v["Number of Positions"], inline=self.embedInline)
             embed.add_embed_field(name="Distance", value=f"{v['Distance']} miles", inline=self.embedInline)
             embed.add_embed_field(name="Closing Date", value=v["Closing Date"], inline=self.embedInline)
             embed.add_embed_field(name="Start Date", value=v["Start Date"], inline=self.embedInline)
             embed.add_embed_field(name="Apprenticeship Level", value=v["Apprenticeship Level"], inline=self.embedInline)
+            embed.add_embed_field(name="Provider", value=v["Provider"], inline=self.embedInline)
             embed.add_embed_field(name="Wage", value=v["Wage"], inline=self.embedInline)
 
+            qualsReqStr = ""
+            for qual in v["Qualifications Required"]:
+                qualsReqStr += f"• {qual}\n"
+            embed.add_embed_field(name="Qualifications Required", value=qualsReqStr, inline=self.embedInline)
+            
             if ("per week" in v["Wage"]):
                 wageInt = int(float(v["Wage"].replace(" per week", "").replace("£", "").replace(",", "")))
                 embed.add_embed_field(name="Wage (per year)", value=f"£{wageInt * 52} per year", inline=self.embedInline)
@@ -123,21 +144,22 @@ class ApprenticeScraper:
             webhook.add_embed(embed)
             resp = webhook.execute()
 
-            print(f"{self.OKBLUE}[{self.now()}][INFO]{self.ENDC} Webhook complete. Code: {resp.status_code}")
+            webhook = DiscordWebhook(url=self.discordUrl, content=v["Full Description"])
+            resp2 = webhook.execute()
+
+            print(f"{self.OKBLUE}[{self.now()}][INFO]{self.ENDC} Webhooks complete. Codes: {resp.status_code}, {resp2.status_code}")
     
     def sendLogWebhook(self, err):
-        if type(err) == ConnectionError:
-            webhook = DiscordWebhook(url=self.discordUrl, content=f"<@370886991065382912> Connection error, check logs for more info.")
-            embed = DiscordEmbed(title="Connection Error", description="", color=0xFF0000)
-            embed.set_timestamp()
-            embed.add_embed_field(name="Error", value=f"{err}", inline=self.embedInline)
-            print(err)
+        webhook = DiscordWebhook(url=self.discordUrl, content=f"<@370886991065382912>Connection error, check logs for more info.")
+        embed = DiscordEmbed(title="Connection Error", description="", color=0xFF0000)
+        embed.set_timestamp()
+        embed.add_embed_field(name="Error", value=f"{err}", inline=self.embedInline)
+        print(err)
 
-            webhook.add_embed(embed)
-            resp = webhook.execute()
-            print(f"{self.OKBLUE}[{self.now()}][INFO]{self.ENDC} Error Logging Webhook complete. Code: {resp.status_code}")
+        webhook.add_embed(embed)
+        resp = webhook.execute()
+        print(f"{self.OKBLUE}[{self.now()}][INFO]{self.ENDC} Error Logging Webhook completed, code: {resp.status_code}")
             
-
     def saveJSON(self, data):
         newData = {}
         compData = self.loadJSON()
@@ -170,17 +192,45 @@ class ApprenticeScraper:
             return {}
 
     def formatStr(self, string):
-        if "positions available" in string or "position available" in string:
-            return string.lstrip().rstrip().replace("- ", "").replace(")", "").replace("position    available", "").replace("s", "")
+        if "position available" in string:
+            string = string.lstrip().rstrip().replace("- ", "").replace(")", "").replace("position    available", "").replace("s", "")
+            return string[:4] + "s" + string[4:]
+        elif "positions available" in string:
+            return string.lstrip().rstrip().replace("- ", "").replace(")", "").replace("position    available", "")
         elif "Apprenticeship level: " in string:
             return string.lstrip().rstrip().replace("Apprenticeship level: ", "")
         elif "Wage: " in string:
             return string.lstrip().rstrip().replace("Wage: ", "")
+        elif "•" in string:
+            return string.lstrip().rstrip().replace("•", "\n•")
 
         return string.lstrip().rstrip()
 
     def now(self):
         return datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+    def makeRequest(self, url, secondary = False):
+        print(f"{self.OKBLUE}[{self.now()}][INFO]{self.ENDC} Making web request") if not secondary else print(f"{self.OKBLUE}[{self.now()}][INFO]{self.ENDC} Making secondary web request")
+        try:
+            r = requests.get(url)
+        except ConnectionError as err:
+            print(f"{self.FAIL}[{self.now()}][ERROR]{self.ENDC} Connection error (Rate Limited)")
+            self.sendLogWebhook(err)
+            return
+        print(f"{self.OKBLUE}[{self.now()}][INFO]{self.ENDC} Web request completed") if not secondary else print(f"{self.OKBLUE}[{self.now()}][INFO]{self.ENDC} Secondary web request completed")
+        secondary = False
+        return r
+
+    def boldStr(self, str, subStr):
+        startPos = str.index(subStr)
+        endPos = startPos + len(subStr) + 3
+
+        if startPos != 0:
+            str = str[:startPos] + "\n**" + str[startPos:]
+        else: 
+            str = str[:startPos] + "**" + str[startPos:]
+
+        return str[:endPos] + "**\n" + str[endPos:]
 
 if __name__ == "__main__":
     ApprenticeScraper()
